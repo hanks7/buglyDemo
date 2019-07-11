fe3fd0c994c917f587a8c0f138def10d6da64070
hjj <474664736@qq.com>
2019-07-11 14:25:51 +0800

    Initial commit

---
+++ animationdemo/src/main/java/com/home/onedemo/activity/MainActivity.java
@@ -0,0 +1,27 @@
+package com.home.onedemo.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.home.onedemo.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnClick(View view) {
+        showToast("2222");
+    }
+
+    private void showToast(String str) {
+        Toast.makeText(this, str + "", Toast.LENGTH_SHORT).show();
+    }
+
+}
---
+++ animationdemo/src/main/java/com/home/onedemo/view/CustomSeekbar.java
@@ -0,0 +1,8 @@
+package com.home.onedemo.view;
+
+/**
+ * Created by wangtao on 2018/4/16.
+ */
+
+public class CustomSeekbar {
+}
